3573c9cf7076ac6550b6f2752e7b34a6f10fb270
ra100 <git@ra100.net>
2020-07-01 23:23:29 +0200

    🔥 Remove unnecessary files from feature-viewer

---
+++ frontend/feature-viewer/index.js
@@ -0,0 +1,18 @@
+/*
+ * feature-viewer
+ * https://github.com/calipho-sib/feature-viewer
+ *
+ * Copyright (c) 2015 Calipho - SIB
+ * Licensed under the GNU GPL v2 license.
+ */
+
+/**
+ @class FeatureViewer
+ */
+
+global.jQuery = $ = require('jquery')
+d3 = require('d3')
+
+var FeatureViewer = require('./src/feature-viewer.js')
+
+module.exports = FeatureViewer
