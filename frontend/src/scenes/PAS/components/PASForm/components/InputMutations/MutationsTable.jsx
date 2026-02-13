0362023824875c0be89d80e535857b924c949f37
ra100 <git@ra100.net>
2026-02-13 23:08:51 +0100

    🐛 Fix CI dependency conflicts and remove @ant-design/compatible
    - Remove unused @ant-design/compatible package (required antd 3.x)
    - Rewrite MutationsTable to use antd 4 Form API directly
    - Add --legacy-peer-deps for npm install in CI for recompose

--- frontend/src/scenes/PAS/components/PASForm/components/InputMutations/MutationsTable.jsx
+++ frontend/src/scenes/PAS/components/PASForm/components/InputMutations/MutationsTable.jsx
@@ -16,331 +16,307 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react'
-import { CloseOutlined } from '@ant-design/icons';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Table, Input, InputNumber, Popconfirm, Button } from 'antd';
+import React, { useState, useEffect, useRef, useContext, useCallback } from 'react'
+import { CloseOutlined } from '@ant-design/icons'
+import { Form, Table, Input, InputNumber, Popconfirm, Button } from 'antd'
 import {
   aminoAcidToCodons,
-  codonTableIncludes
-} from "shared/genes";
+  codonTableIncludes,
+} from 'shared/genes'
 
 import '../../../styles.css'
 
-const EditableContext = React.createContext();
+const EditableContext = React.createContext(null)
 
-const EditableRow = ({ form, index, ...props }) => (
-  <EditableContext.Provider value={form}>
-    <tr {...props} />
-  </EditableContext.Provider>
-);
-
-const EditableFormRow = Form.create()(EditableRow);
-
-class EditableCell extends React.Component {
-  state = {
-    editing: true,
-  };
-
-  toggleEdit = () => {
-    const editing = !this.state.editing;
-    this.setState({ editing }, () => {
-      if (editing) {
-        this.input.focus()
-      }
-    })
-  };
-
-  save = e => {
-    e.preventDefault();
-    const { record, handleSave } = this.props;
-    this.form.validateFields((error, values) => {
-      this.toggleEdit();
-      handleSave({ ...record, ...values })
-    })
-  };
-
-  renderCell = form => {
-    this.form = form;
-    const { children, dataIndex, record, title } = this.props;
-    const { editing } = this.state;
-    return editing ? (
-      <Form.Item style={{ margin: 0 }}>
-        {form.getFieldDecorator(dataIndex, {
-          rules: [
-            {
-              required: true,
-              message: `Please Input ${title}`,
-            },
-            {validator: this.validateTarget},
-            {validator: this.validateMT},
-            {validator: this.validateMTP},
-          ],
-          initialValue: record[dataIndex],
-        })(
-          this.props.inputType === 'number' ?
-            <InputNumber ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} /> :
-            <Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />
-          )}
-      </Form.Item>
-    ) : (
-      <div
-        className="editable-cell-value-wrap"
-        style={{ paddingRight: 24 }}
-        onClick={this.toggleEdit}
-      >
-        {children}
-      </div>
-    )
-  };
-
-  validateTarget = (rule, value, callback) => {
-    if (value && value.length > 1 && rule.field === 'target') {
-      const sequence = document.getElementById('goiSequence').value;
-      const sequenceTypeSelector = document.getElementsByName('sequenceRadioButton')[0];
-      let sequenceType = 'dna';
-      if (sequenceTypeSelector.value === 'dna') {
-        if (sequenceTypeSelector.checked === true) sequenceType = 'dna';
-        else sequenceType = 'protein'
-      }
-      const targetValue = value.charAt(0).toUpperCase();
-      const position = parseInt(value.slice(1));
-      const errorStr = `Target ${targetValue} is not present at site ${position}.
-          Current value is `;
-      if(sequenceType === "protein") {
-        if (sequence.charAt(position-1) === targetValue) {
-          callback()
-        } else {
-          callback(errorStr)
-        }
-      } else {
-        if (codonTableIncludes(targetValue) && aminoAcidToCodons(targetValue)
-          .includes(sequence.substr((position-1)*3, 3))) {
-          callback()
-        } else {
-          callback(errorStr)
-        }
-      }
-    } else {
-      callback()
-    }
-  };
-
-  validateMT = (rule, value, callback) => {
-    if (value && rule.field === 'mt') {
-      const mutationsTypeSelector = document.getElementsByName('mutationsRadioButton')[0];
-      let mutationsType = 'dna';
-      if (mutationsTypeSelector.value === 'dna') {
-        if (mutationsTypeSelector.checked === true) mutationsType = 'dna';
-        else mutationsType = 'protein'
-      }
-        if (mutationsType === 'dna') {
-          if (!(value || '').match(/^[ACGTacgt, ]+$/)) {
-            callback('Allowed values are: '
-              + 'A, C, T, G')
-          } else {
-            callback()
-          }
-        } else {
-        if (!(value || '').match(/^[FLIMVSGTAYHQNKDECWRPflimvsgtayhqnkdecwrp, ]+$/)) {
-          callback('Allowed values are: '
-            + 'F, L, I, M, V, S, G, T, A, Y, H, Q, N, K, D, E, C, W, R, P')
-        } else {
-          callback()
-        }
-      }
-    } else {
-      callback()
-    }
-  };
-
-  validateMTP = (rule, value, callback) => {
-    if (value && rule.field === 'mtp') {
-      if (0 < value && value <= 100) callback();
-      else callback('MT% must be between 0 and 100')
-    } else {
-      callback()
-    }
-  };
-
-  render() {
-    const {
-      editable,
-      dataIndex,
-      title,
-      record,
-      index,
-      handleSave,
-      children,
-      ...restProps
-    } = this.props;
-    return (
-      <td {...restProps}>
-        {editable ? (
-          <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
-        ) : (
-          children
-        )}
-      </td>
-    )
-  }
+const EditableRow = ({ index, ...props }) => {
+  const [form] = Form.useForm()
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  )
 }
 
-class EditableTable extends React.Component {
-  constructor(props) {
-    super(props);
-    const mutations = props.mutations;
+const EditableCell = ({
+  editable,
+  dataIndex,
+  title,
+  record,
+  index,
+  handleSave,
+  children,
+  inputType,
+  ...restProps
+}) => {
+  const [editing, setEditing] = useState(true)
+  const inputRef = useRef(null)
+  const form = useContext(EditableContext)
 
-    this.state = {
-      data: mutations,
-      editingKey: '',
-      count: 0,
-    };
+  useEffect(() => {
+    if (editing && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [editing])
 
-    this.columns = [
-      {
-        title: 'Target Residue',
-        dataIndex: 'target',
-        width: '35%',
-        editable: true,
-      },
-      {
-        title: 'MT',
-        dataIndex: 'mt',
-        width: '25%',
-        editable: true,
-      },
-      {
-        title: 'MT%',
-        dataIndex: 'mtp',
-        width: '35%',
-        editable: true,
-      },
-      {
-        title: '',
-        dataIndex: 'operation',
-        width: '5%',
-        render: (text, record) =>
-          this.state.data.length >= 1 ? (
-            <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.key)}>
-              <Button type="danger" shape="circle" icon={<CloseOutlined />} />
-            </Popconfirm>
-          ) : null,
+  const validateTarget = (_, value) => {
+    if (value && value.length > 1 && dataIndex === 'target') {
+      const sequence = document.getElementById('goiSequence')?.value || ''
+      const sequenceTypeSelector = document.getElementsByName('sequenceRadioButton')[0]
+      let sequenceType = 'dna'
+      if (sequenceTypeSelector) {
+        if (sequenceTypeSelector.value === 'dna' && sequenceTypeSelector.checked) {
+          sequenceType = 'dna'
+        } else {
+          sequenceType = 'protein'
+        }
       }
-    ]
-  }
-
-  static getDerivedStateFromProps(nextProps) {
-    // Should be a controlled component.
-    if ('value' in nextProps) {
-      return {
-        ...(nextProps.value || {}),
+      const targetValue = value.charAt(0).toUpperCase()
+      const position = parseInt(value.slice(1))
+      const errorStr = `Target ${targetValue} is not present at site ${position}. Current value is `
+      if (sequenceType === 'protein') {
+        if (sequence.charAt(position - 1) === targetValue) {
+          return Promise.resolve()
+        }
+        return Promise.reject(new Error(errorStr))
+      } else {
+        if (
+          codonTableIncludes(targetValue) &&
+          aminoAcidToCodons(targetValue).includes(sequence.substr((position - 1) * 3, 3))
+        ) {
+          return Promise.resolve()
+        }
+        return Promise.reject(new Error(errorStr))
       }
     }
-    return null
+    return Promise.resolve()
   }
 
-  isEditing = record => record.key === this.state.editingKey;
-
-  triggerChange = changedValue => {
-    // Should provide an event to pass value to Form.
-    const { onChange } = this.props;
-    if (onChange) {
-      onChange({
-        ...changedValue,
-      })
+  const validateMT = (_, value) => {
+    if (value && dataIndex === 'mt') {
+      const mutationsTypeSelector = document.getElementsByName('mutationsRadioButton')[0]
+      let mutationsType = 'dna'
+      if (mutationsTypeSelector) {
+        if (mutationsTypeSelector.value === 'dna' && mutationsTypeSelector.checked) {
+          mutationsType = 'dna'
+        } else {
+          mutationsType = 'protein'
+        }
+      }
+      if (mutationsType === 'dna') {
+        if (!(value || '').match(/^[ACGTacgt, ]+$/)) {
+          return Promise.reject(new Error('Allowed values are: A, C, T, G'))
+        }
+        return Promise.resolve()
+      } else {
+        if (!(value || '').match(/^[FLIMVSGTAYHQNKDECWRPflimvsgtayhqnkdecwrp, ]+$/)) {
+          return Promise.reject(
+            new Error('Allowed values are: F, L, I, M, V, S, G, T, A, Y, H, Q, N, K, D, E, C, W, R, P')
+          )
+        }
+        return Promise.resolve()
+      }
     }
-  };
+    return Promise.resolve()
+  }
 
-  handleSave = row => {
-    const newData = [...this.state.data];
-    const index = newData.findIndex(item => row.key === item.key);
-    const item = newData[index];
+  const validateMTP = (_, value) => {
+    if (value !== undefined && dataIndex === 'mtp') {
+      if (0 < value && value <= 100) {
+        return Promise.resolve()
+      }
+      return Promise.reject(new Error('MT% must be between 0 and 100'))
+    }
+    return Promise.resolve()
+  }
+
+  const save = async () => {
+    try {
+      const values = await form.validateFields()
+      handleSave({ ...record, ...values })
+    } catch {
+      // ignore
+    }
+  }
+
+  const getInputComponent = () => {
+    if (inputType === 'number') {
+      return (
+        <InputNumber
+          ref={inputRef}
+          onPressEnter={save}
+          onBlur={save}
+        />
+      )
+    }
+    return (
+      <Input
+        ref={inputRef}
+        onPressEnter={save}
+        onBlur={save}
+      />
+    )
+  }
+
+  return (
+    <td {...restProps}>
+      {editable ? (
+        <Form form={form} component={false} initialValues={{ [dataIndex]: record[dataIndex] }}>
+          <EditableContext.Provider value={form}>
+            {editing ? (
+              <Form.Item
+                style={{ margin: 0 }}
+                name={dataIndex}
+                rules={[
+                  {
+                    required: true,
+                    message: `Please Input ${title}`,
+                  },
+                  { validator: validateTarget },
+                  { validator: validateMT },
+                  { validator: validateMTP },
+                ]}
+                initialValue={record[dataIndex]}
+              >
+                {getInputComponent()}
+              </Form.Item>
+            ) : (
+              <div
+                className="editable-cell-value-wrap"
+                style={{ paddingRight: 24 }}
+                onClick={() => setEditing(true)}
+              >
+                {children}
+              </div>
+            )}
+          </EditableContext.Provider>
+        </Form>
+      ) : (
+        children
+      )}
+    </td>
+  )
+}
+
+const EditableTable = ({ mutations, onChange }) => {
+  const [data, setData] = useState(mutations || [])
+  const [count, setCount] = useState(mutations?.length || 0)
+
+  useEffect(() => {
+    if (mutations) {
+      setData(mutations)
+      setCount(mutations.length)
+    }
+  }, [mutations])
+
+  const triggerChange = useCallback(
+    (newData) => {
+      if (onChange) {
+        onChange({ mutations: newData })
+      }
+    },
+    [onChange]
+  )
+
+  const handleSave = (row) => {
+    const newData = [...data]
+    const index = newData.findIndex((item) => row.key === item.key)
+    const item = newData[index]
     newData.splice(index, 1, {
       ...item,
       ...row,
-    });
-    this.setState({ data: newData });
-    this.triggerChange({mutations: newData})
-  };
-
-  handleAdd = () => {
-    const {count, data} = this.state;
-    const newData = {
-      key: count
-    };
-    this.setState({
-      data: [...data, newData],
-      count: count + 1,
     })
-  };
-
-  handleDelete = key => {
-    const dataSource = [...this.state.data];
-    const filteredDataSource = dataSource.filter(item => item.key !== key);
-    this.setState({
-      data: filteredDataSource
-    });
-    this.triggerChange({mutations: filteredDataSource})
-  };
-
-  componentDidUpdate = (prevProps, prevState, snapshot) => {
-    // Check if the suplied props is changed
-    if (prevProps.mutations !== this.props.mutations) {
-      // run the function with the suplied new property
-      this.setState({
-        data: this.props.mutations,
-        count: this.props.mutations.length,
-      })
-    }
-  };
-
-  render() {
-    const components = {
-      body: {
-        row: EditableFormRow,
-        cell: EditableCell,
-      }
-    };
-
-    const columns = this.columns.map(col => {
-      if (!col.editable) {
-        return col
-      }
-      return {
-        ...col,
-        onCell: record => ({
-          record,
-          inputType: col.dataIndex === 'mtp' ? 'number' : 'text',
-          dataIndex: col.dataIndex,
-          title: col.title,
-          editable: col.editable,
-          handleSave: this.handleSave
-        }),
-      }
-    });
-
-    return (
-      <div>
-        <EditableContext.Provider value={this.props.form}>
-          <Table
-            components={components}
-            bordered
-            dataSource={this.state.data}
-            columns={columns}
-            rowClassName="editable-row"
-            pagination={false}
-          />
-          <Button onClick={this.handleAdd} type="primary"
-                  style={{marginBottom: 16}}>
-            Add a row
-          </Button>
-        </EditableContext.Provider>
-      </div>
-    )
+    setData(newData)
+    triggerChange(newData)
   }
+
+  const handleAdd = () => {
+    const newData = {
+      key: count,
+    }
+    const updatedData = [...data, newData]
+    setData(updatedData)
+    setCount(count + 1)
+  }
+
+  const handleDelete = (key) => {
+    const filteredDataSource = data.filter((item) => item.key !== key)
+    setData(filteredDataSource)
+    triggerChange(filteredDataSource)
+  }
+
+  const columns = [
+    {
+      title: 'Target Residue',
+      dataIndex: 'target',
+      width: '35%',
+      editable: true,
+    },
+    {
+      title: 'MT',
+      dataIndex: 'mt',
+      width: '25%',
+      editable: true,
+    },
+    {
+      title: 'MT%',
+      dataIndex: 'mtp',
+      width: '35%',
+      editable: true,
+    },
+    {
+      title: '',
+      dataIndex: 'operation',
+      width: '5%',
+      render: (_, record) =>
+        data.length >= 1 ? (
+          <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
+            <Button type="primary" danger shape="circle" icon={<CloseOutlined />} />
+          </Popconfirm>
+        ) : null,
+    },
+  ]
+
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  }
+
+  const mappedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.dataIndex === 'mtp' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editable: col.editable,
+        handleSave,
+      }),
+    }
+  })
+
+  return (
+    <div>
+      <Table
+        components={components}
+        bordered
+        dataSource={data}
+        columns={mappedColumns}
+        rowClassName="editable-row"
+        pagination={false}
+      />
+      <Button onClick={handleAdd} type="primary" style={{ marginBottom: 16 }}>
+        Add a row
+      </Button>
+    </div>
+  )
 }
 
-const EditableFormTable = Form.create()(EditableTable);
-export default EditableFormTable
+export default EditableTable
